9db073bb7837708bcfff5172ae937e27b47226a3
緒方雄介 <ogatayuusuke@ogatayuukenoMBP.lan>
2023-06-10 12:31:20 +0900

    add auth

---
+++ src/chat/auth.go
@@ -0,0 +1,54 @@
+package main
+import (
+	"net/http"
+	"strings"
+	"log"
+	"fmt"
+	"github.com/stretchr/gomniauth"
+)
+
+type authHandler struct {
+	next http.Handler
+}
+
+func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if _, err := r.Cookie("auth"); err == http.ErrNoCookie {
+		// 未承認
+		w.Header().Set("Location", "/login")
+		w.WriteHeader(http.StatusTemporaryRedirect)
+ 	} else if err != nil {
+		// 何らか別エラーが発生
+		panic(err.Error())
+	} else {
+		// 成功。ラップされたハンドラを呼び出します。
+		h.next.ServeHTTP(w, r)
+	}
+}
+
+func MustAuth(handler http.Handler) http.Handler {
+	return &authHandler{next: handler}
+}
+
+// loginHandlerはサードパーティーへのログインの処理を受け持ちます。
+// パスの形式: /auth/{action}/{provider}
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	segs := strings.Split(r.URL.Path, "/")
+	action := segs[2]
+	provider := segs[3]
+	switch action {
+	case "login":
+		provider, err := gomniauth.Provider(provider)
+		if err != nil {
+			log.Fatalln("認証プロバイダーの取得に失敗しました：", provider, "-", err)
+		}
+		loginUrl, err := provider.GetBeginAuthURL(nil, nil)
+		if err != nil {
+			log.Fatalln("GetBeginAuthUrlの呼び出し中にエラーが発生しました。", provider, "-", err)
+		}
+		w.Header().Set("Location",loginUrl)
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "アクション%sには非対応です", action)
+	}
+} 
