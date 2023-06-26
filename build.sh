3b25f9d531820a867ddbe0d1b838a834286b532d
緒方雄介 <ogatayuusuke@ogatayuukenoMBP.lan>
2023-06-26 16:44:04 +0900

    fix

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t https://os3-318-48873.vs.sakura.ne.jp:447/sandbox/golang/$DRONE_BRANCH .
-docker push https://os3-318-48873.vs.sakura.ne.jp:447/sandbox/golang/$DRONE_BRANCH
+# docker build -t https://os3-318-48873.vs.sakura.ne.jp:447/sandbox/golang/$DRONE_BRANCH .
+docker push https://os3-318-48873.vs.sakura.ne.jp:447/sandbox/golang/$DRONE_BRANCH:latest
